a06b945e044e42785b0ff5edbd11f88d4aae82db
nike0good <wuhang@wuhangdeMacBook-Pro.local>
2019-05-26 03:05:51 +0800

    [fix] error:Path was not declared in this scope

--- src/bar/bar_driver.cpp
+++ src/bar/bar_driver.cpp
@@ -47,6 +47,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "cpp_common/pgr_alloc.hpp"
 #include "cpp_common/pgr_assert.h"
+#include "cpp_common/basePath_SSEC.hpp"
 
 template < class G >
 std::deque< Path >
